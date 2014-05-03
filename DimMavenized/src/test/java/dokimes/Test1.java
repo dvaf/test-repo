a1e1e6b8d57bac77aebdb8c962b7bac0ab2e3d4b
dvaf <dvaf12@gmail.com>
2014-05-03 08:17:19 +0300

    1st local

---
+++ DimMavenized/src/test/java/dokimes/Test1.java
@@ -0,0 +1,17 @@
+package dokimes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void testNumberOne() {
+//		fail("Not yet implemented");
+		
+		
+		assertEquals(1, 2);
+	}
+
+}
